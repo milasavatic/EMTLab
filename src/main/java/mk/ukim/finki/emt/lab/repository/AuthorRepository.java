7b90280ec24832abfaff19b4981f961ab6eb7a1f
milasavatic <milasavatic48@gmail.com>
2025-04-22 01:13:31 +0200

    Initial commit

--- src/main/java/mk/ukim/finki/emt/lab/repository/AuthorRepository.java
+++ src/main/java/mk/ukim/finki/emt/lab/repository/AuthorRepository.java
@@ -3,11 +3,9 @@ package mk.ukim.finki.emt.lab.repository;
 import mk.ukim.finki.emt.lab.model.domain.Author;
 import mk.ukim.finki.emt.lab.model.projections.AuthorNameProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
